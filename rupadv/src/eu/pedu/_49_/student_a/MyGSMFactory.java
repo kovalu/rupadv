6b3c615dfb69ea3288b8ee832b10621e70587dc4
kovalu <kovarik.lubos@seznam.cz>
2015-12-08 22:43:45 +0100

    Kořenový balíček přejmenován podle knihy na eu.pedu._49_.student_a

---
+++ rupadv/src/eu/pedu/_49_/student_a/MyGSMFactory.java
@@ -0,0 +1,109 @@
+/* The file is saved in UTF-8 codepage.
+ * Check: «Stereotype», Section mark-§, Copyright-©, Alpha-α, Beta-β, Smile-☺
+ */
+package eu.pedu._49_.student_a;
+
+import eu.pedu.adv15p_fw.game_txt.IGSMFactory;
+import eu.pedu.adv15p_fw.scenario.AScenarioManager;
+
+
+
+/*******************************************************************************
+ * Instance třídy {@code EmptyGSMFactory} představují tovární objekty,
+ * které jsou schopny na požádání dodat instanci hry,
+ * instanci správce scénářů této hry
+ * a instanci textového uživatelského rozhraní.
+ * Dokud ještě některé z těchto objektů nejsou definovány,
+ * vyhazují metody výjimku {@link UnsupportedOperationException}.
+ * <p>
+ * V první fázi vývoje celé aplikace bývá aktivní pouze metoda
+ * {@link #getScenarioManager()} umožňující získání instance správce scénářů
+ * vyvíjené hry a těla zbylých metod bývají zakomentovaná.
+ * Posléze po odkomentování zakomentovaných těl metod lze postupně získat
+ * také vlastní textové i grafické verze hry
+ * a jejího textového, resp. grafického uživatelského rozhraní.
+ *
+ * @author  Rudolf PECINOVSKÝ
+ * @version 2015-Podzim
+ */
+public class MyGSMFactory implements IGSMFactory, IAuthorMe
+{
+//== CONSTANT CLASS ATTRIBUTES =================================================
+//== VARIABLE CLASS ATTRIBUTES =================================================
+
+
+
+//##############################################################################
+//== STATIC INITIALIZER (CLASS CONSTRUCTOR) ====================================
+//== CLASS GETTERS AND SETTERS =================================================
+//== OTHER NON-PRIVATE CLASS METHODS ===========================================
+//== PRIVATE AND AUXILIARY CLASS METHODS =======================================
+
+
+
+//##############################################################################
+//== CONSTANT INSTANCE ATTRIBUTES ==============================================
+//== VARIABLE INSTANCE ATTRIBUTES ==============================================
+
+
+
+//##############################################################################
+//== CONSTUCTORS AND FACTORY METHODS ===========================================
+
+    /***************************************************************************
+     * Vytvoří tovární objekt poskytující klíčové objekty aplikace.
+     */
+    public MyGSMFactory()
+    {
+    }
+
+
+
+//== ABSTRACT METHODS ==========================================================
+//== INSTANCE GETTERS AND SETTERS ==============================================
+
+    /***************************************************************************
+     * Vrátí odkaz na instanci správce scénářů.
+     *
+     * @return Požadovaný odkaz
+     */
+//    @Override
+//    public AScenarioManager getScenarioManager()
+//    {
+//        return ScenarioManager.getInstance();
+//    }
+
+
+    /***************************************************************************
+     * Vrátí odkaz na instanci textové verze hry.
+     *
+     * @return Požadovaný odkaz
+     */
+    @Override
+    public MyGame getGame()
+    {
+        return MyGame.getInstance();
+    }
+
+
+    /***************************************************************************
+     * Vrátí odkaz na instanci třídy realizující uživatelské rozhraní.
+     *
+     * @return Požadovaný odkaz
+     */
+//    @Override
+//    public IUI getUI()
+//    {
+//        return TextUI_Instance;
+//    }
+
+
+
+//== OTHER NON-PRIVATE INSTANCE METHODS ========================================
+//== PRIVATE AND AUXILIARY INSTANCE METHODS ====================================
+
+
+
+//##############################################################################
+//== NESTED DATA TYPES =========================================================
+}
---
+++ rupadv/src/eu/pedu/_49_/student_a/MyGame.java
@@ -0,0 +1,217 @@
+/* The file is saved in UTF-8 codepage.
+ * Check: «Stereotype», Section mark-§, Copyright-©, Alpha-α, Beta-β, Smile-☺
+ */
+package eu.pedu._49_.student_a;
+
+import eu.pedu.adv15p_fw.game_txt.BasicActions;
+import eu.pedu.adv15p_fw.game_txt.IAction;
+import eu.pedu.adv15p_fw.game_txt.IBag;
+import eu.pedu.adv15p_fw.game_txt.IGame;
+import eu.pedu.adv15p_fw.game_txt.IWorld;
+
+import java.util.Collection;
+
+
+
+/*******************************************************************************
+ * Instance třídy {@code EmptyGame} mají na starosti logiku hry.
+ * Jsou schopny akceptovat jednotlivé příkazy a poskytovat informace
+ * o průběžném stavu hry a jejích součástí.
+ * <p>
+ * Třída hry musí být definována jako jedináček (singleton)
+ * a kromě metod deklarovaných v interfejsu {@link IGame} musí definovat
+ * statickou tovární metodu {@code getInstance()}.
+ * Splnění této podmínky nemůže prověřit překladač,
+ * ale prověří ji až následné testy hry.
+ * <p>
+ * Instance třídy {@code EmptyGame} představují prototypy instancí hry,
+ * které ještě nejsou schopny plnohodnotného spuštění a slouží pouze
+ * ke kompletaci těch vlastností správce scénářů, které bude v budoucnu
+ * plnit ve spolupráci s plnohodnotnou hrou.
+ *
+ * @author  Rudolf PECINOVSKÝ
+ * @version 2015-Podzim
+ */
+public class MyGame extends ANamed implements IGame, IAuthorMe
+{
+//== CONSTANT CLASS ATTRIBUTES =================================================
+
+    /** Tovární třída, jejíž instancemi jsou tovární objekty poskytující
+     *  instanci správce scénářů i hry, jejíž scénáře daný správce spravuje. */
+    private final static Class<MyGSMFactory> FACTORY_CLASS = MyGSMFactory.class;
+
+    /** Odkaz na jedinou instanci (jedináčka) této hry. */
+    private static final MyGame SINGLETON = new MyGame();
+
+
+
+//== VARIABLE CLASS ATTRIBUTES =================================================
+
+
+
+//##############################################################################
+//== STATIC INITIALIZER (CLASS CONSTRUCTOR) ====================================
+//== CLASS GETTERS AND SETTERS =================================================
+//== OTHER NON-PRIVATE CLASS METHODS ===========================================
+//== PRIVATE AND AUXILIARY CLASS METHODS =======================================
+
+
+
+//##############################################################################
+//== CONSTANT INSTANCE ATTRIBUTES ==============================================
+//== VARIABLE INSTANCE ATTRIBUTES ==============================================
+
+
+
+//##############################################################################
+//== CONSTUCTORS AND FACTORY METHODS ===========================================
+
+    /***************************************************************************
+     * Tovární metoda vracející odkaz na jedninou existující instanci dané hry.
+     *
+     * @return Instance dané hry
+     */
+    public static MyGame getInstance()
+    {
+        return SINGLETON;
+    }
+
+
+    /***************************************************************************
+     * Soukromý konstruktor definující jedinou instanci.
+     * Protože je soukromý, musí být definován, i když má prázdné tělo.
+     */
+    private MyGame()
+    {
+        super("My game");
+    }
+
+
+
+//== ABSTRACT METHODS ==========================================================
+//== INSTANCE GETTERS AND SETTERS ==============================================
+
+    /***************************************************************************
+     * Vrátí class-objekt tovární třídy, jejíž instance umějí zprostředkovat
+     * získání všech klíčových objektů aplikace.
+     *
+     * @return Class-objekt tovární třídy
+     */
+    @Override
+    public Class<MyGSMFactory> getFactoryClass()
+    {
+        return FACTORY_CLASS;
+    }
+
+
+    /***************************************************************************
+     * Vrátí informaci o tom, je-li hra aktuálně spuštěná.
+     * Spuštěnou hru není možno pustit znovu.
+     * Chceme-li hru spustit znovu, musíme ji nejprve ukončit.
+     *
+     * @return Je-li hra spuštěná, vrátí {@code true},
+     *         jinak vrátí {@code false}
+     */
+    @Override
+    public boolean isAlive()
+    {
+        //TODO EmptyGame.isAlive - Metoda ještě není hotova
+        throw new UnsupportedOperationException("\nMetoda ještě není hotova.");
+    }
+
+
+    /***************************************************************************
+     * Vrátí odkaz na batoh, do nějž bude hráč ukládat sebrané předměty.
+     *
+     * @return Batoh, do nějž hráč ukládá sebrané předměty
+     */
+    @Override
+    public IBag getBag()
+    {
+        //TODO EmptyGame.getBag - Metoda ještě není hotova
+        throw new UnsupportedOperationException("\nMetoda ještě není hotova.");
+    }
+
+
+    /***************************************************************************
+     * Vrátí kolekci všech příkazů použitelných ve hře.
+     *
+     * @return Kolekce všech příkazů použitelných ve hře
+     */
+    @Override
+    public Collection<? extends IAction> getAllActions()
+    {
+        //TODO EmptyGame.getAllActions - Metoda ještě není hotova
+        throw new UnsupportedOperationException("\nMetoda ještě není hotova.");
+    }
+
+
+    /***************************************************************************
+     * Vrátí odkaz na přepravku s názvy povinných příkazů, tj. příkazů pro
+     * <ul>
+     *   <li>přesun hráče do jiného prostoru,</li>
+     *   <li>zvednutí objektu (odebrání z prostoru a vložení do batohu),</li>
+     *   <li>položení objektu (odebrání z batohu a vložení do prostoru),</li>
+     *   <li>vyvolání nápovědy,</li>
+     *   <li>okamžité ukončení hry.</li>
+     * </ul>
+     *
+     * @return Přepravka názvy povinných příkazů
+     */
+    @Override
+    public BasicActions getBasicActions()
+    {
+        //TODO EmptyGame.getBasicActions - Metoda ještě není hotova
+        throw new UnsupportedOperationException("\nMetoda ještě není hotova.");
+    }
+
+
+    /***************************************************************************
+     * Vrátí odkaz na svět, v němž se hra odehrává.
+     *
+     * @return Svět, v němž se hra odehrává
+     */
+    @Override
+    public IWorld getWorld()
+    {
+        //TODO EmptyGame.getWorld - Metoda ještě není hotova
+        throw new UnsupportedOperationException("\nMetoda ještě není hotova.");
+    }
+
+
+
+//== OTHER NON-PRIVATE INSTANCE METHODS ========================================
+
+    /***************************************************************************
+     * Zpracuje zadaný příkaz a vrátí text zprávy pro uživatele.
+     *
+     * @param command Zadávaný příkaz
+     * @return Textová odpověď hry na zadaný příkaz
+     */
+    @Override
+    public String executeCommand(String command)
+    {
+        //TODO EmptyGame.executeCommand(String) - Metoda ještě není hotova
+        throw new UnsupportedOperationException("\nMetoda ještě není hotova.");
+    }
+
+
+    /***************************************************************************
+     * Ukončí celou hru a vrátí alokované prostředky.
+     */
+    @Override
+    public void stop()
+    {
+        //TODO EmptyGame.stop - Metoda ještě není hotova
+        throw new UnsupportedOperationException("\nMetoda ještě není hotova.");
+    }
+
+
+
+//== PRIVATE AND AUXILIARY INSTANCE METHODS ====================================
+
+
+
+//##############################################################################
+//== NESTED DATA TYPES =========================================================
+}
